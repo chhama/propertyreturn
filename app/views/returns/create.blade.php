9fa9c8434e3ec739133a271683e3650d388d3a16
John Dapzar <johndapzar@yahoo.com>
2014-11-19 14:20:39 +0530

    OTP

--- app/views/returns/create.blade.php
+++ app/views/returns/create.blade.php
@@ -4,6 +4,7 @@
 
 @section('container')
     <script language="javascript">
+
         function removeRow(rnum) {
             $('#rowNum'+rnum).remove();
         }
@@ -18,8 +19,10 @@
                 data: {'id': '123'},
                 type: 'GET', 
             }).success(function(data){
+                $('#otp').removeClass('hidden');
+                $('#submitbutton').removeClass('hidden');
                 //console.log('Hello');
-                $('#hello1').html(data);
+                //$('#hello1').html(data);
             })
         }
     </script>
@@ -175,35 +178,16 @@
                             </div>
                         
                         <!-- OTP -->
-                        <div class="form-group {{Input::old('otp') && !$errors->has('otp') ? 'hidden' : ''}}">
-                            <div class="col-md-8 col-md-offset-4">
+                        
+                            <div class="col-md-2">
                                 <button type="button" onclick="return interactionRequestOTP()" id="interaction_request_otp" class="btn btn-success"><i class="fa fa-key"></i> Generate OTP </button>
                             </div>
-                        </div>
-                        <div class="form-group hidden" id="otp_info">
-                            <div class="col-md-8 col-md-offset-4">
-                                <div class="alert alert-info">
-                                    <p> ssa </p>
-                                </div>
+                            <div class="col-md-2">
+                                    {{Form::text('otp',Input::old('otp'),['placeholder'=>'Enter OTP','id'=>'otp','class'=>'form-control hidden','required'])}}
                             </div>
+                        <div class="col-md-2">
+                                {{Form::Submit('Submit',['id'=>'submitbutton','class'=>'btn btn-primary form-control hidden','style'=>'width:120px;'])}}
                         </div>
-                        <div class="form-group {{$errors->has('otp') ? 'has-error' : Input::old('otp') ? '' : 'hidden interaction-hidden'}}">
-                            <label for="otp" class="col-md-4 control-label">One Time Password</label>
-                            <div class="col-md-8">
-                                <?php 
-                                $otpProperties = ['class'=>'form-control'];
-                                // if(Input::old('otp') && !$errors->has('otp'))
-                                //     $otpProperties['readonly'] = 'readonly';
-                                 ?>
-                                {{Form::text('otp',Input::old('otp',''),$otpProperties)}}
-                                {{$errors->has('otp') ? '<span class="help-block">'.$errors->first('otp').'</span>' : '' }}
-                                @if(Input::old('otp') && !$errors->has('otp'))
-                                    <p class="text-info "> <?php echo ('The one time password you entered will be used'); ?> </p>
-                                @endif
-                            </div>
-                        </div>
-                        {{Form::Submit('Submit',['class'=>'btn btn-primary form-control','style'=>'width:120px;'])}}
-                        <div id="hello1">HHH</div>
                     {{Form::close()}}
                     </div>
                 </div>
