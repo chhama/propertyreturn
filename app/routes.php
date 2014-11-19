930d27209c3e38aad4163e90ee12f05e1b0771cc
John Dapzar <johndapzar@yahoo.com>
2014-11-19 12:08:34 +0530

    SMS Thon theih loh

--- app/routes.php
+++ app/routes.php
@@ -21,5 +21,8 @@ Route::resource('users','UsersController');
 Route::post('login', array('uses'=>'UsersController@login','as'=>'login'));
 Route::get('users/{id}/changepassword', array('uses'=>'UsersController@changePassword','as'=>'users.changepassword'));
 Route::get('users/{id}/profile', array('uses'=>'UsersController@profile','as'=>'users.profile'));
+Route::get('property/{id}/otp', array('uses'=>'PropertyController@otp','as'=>'property.otp'));
+
+//Route::get('otp', function(){ dd('Hello'); });
 
 Route::get('getreturns', ['uses'=>'PropertyController@getreturns','as'=>'getreturns']);
