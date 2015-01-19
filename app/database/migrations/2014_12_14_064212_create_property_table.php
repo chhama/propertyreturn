01855ec4a40765c2d677c8ad5ccb0448be7abb48
John Dapzar <johndapzar@yahoo.com>
2015-01-19 12:43:47 +0530

    Property Status Added

--- app/database/migrations/2014_12_14_064212_create_property_table.php
+++ app/database/migrations/2014_12_14_064212_create_property_table.php
@@ -24,6 +24,7 @@ class CreatePropertyTable extends Migration {
 			$table->integer('returns_year')->length(4);
 			$table->longtext('movable_property');
 			$table->longtext('immovable_property');
+			$table->string('status');
 			$table->timestamps();
 		});
 	}
